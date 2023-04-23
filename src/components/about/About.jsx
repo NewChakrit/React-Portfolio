06596acd0038616948f114bd428d9cab3f0ecb94
A657002 A657002 <a657002@AR8040657002N1.local>
2023-04-23 18:07:27 +0700

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,69 @@
 import React from "react";
 import "./about.css";
+import Me from "../../assets/avatar-2.svg";
+import AboutBox from "./AboutBox";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="about container section" id="about">
+      <h2 className="section__title">About Me</h2>
+
+      <div className="about__container grid">
+        <img src={Me} alt="" className="about__img" />
+
+        <div className="about__data grid">
+          <div className="about__info">
+            <p className="about__description">
+              I am a skilled full stack developer. I am a quick learner and a
+              team worker that gets the job done. I can easily capitalize on low
+              hanging fruits and quickly maximize timely deliverables for
+              real-time schemas.
+            </p>
+            <a href="" className="btn">
+              Download CV
+            </a>
+          </div>
+
+          <div className="about__skills grid">
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Development</h3>
+                <span className="skills__number ">90%</span>
+              </div>
+
+              <div className="skills__bar">
+                <span className="skills__percentage development"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">UI/UX design</h3>
+                <span className="skills__number ">80%</span>
+              </div>
+
+              <div className="skills__bar">
+                <span className="skills__percentage ui__design"></span>
+              </div>
+            </div>
+
+            <div className="skills__data">
+              <div className="skills__titles">
+                <h3 className="skills__name">Photography</h3>
+                <span className="skills__number">60%</span>
+              </div>
+
+              <div className="skills__bar">
+                <span className="skills__percentage photography"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <AboutBox />
+    </section>
+  );
 };
 
 export default About;
