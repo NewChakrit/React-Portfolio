a70abbfee7d305cc2769c975e8976cc86f127933
A657002 A657002 <a657002@AR8040657002N1.local>
2023-04-26 14:35:37 +0700

    experience

--- src/components/resume/Card.jsx
+++ src/components/resume/Card.jsx
@@ -6,7 +6,10 @@ const Card = (props) => {
       <i className={props.icon}></i>
       <span className="timeline__date">{props.year}</span>
       <h3 className="timeline__title">{props.title}</h3>
+      <p className="timeline__text">{props.title2}</p>
+      <br />
       <p className="timeline__text">{props.desc}</p>
+      <br />
     </div>
   );
 };
