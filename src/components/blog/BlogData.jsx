8d2cfba8b731a98dd0869243d1929e46cc602766
A657002 A657002 <a657002@AR8040657002N1.local>
2023-04-27 16:50:54 +0700

    Technical Skills

---
+++ src/components/blog/BlogData.jsx
@@ -0,0 +1,144 @@
+const BlogData = [
+  {
+    id: 1,
+    category: "Html",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1646850887/Logo/html5_cykxyz.png",
+  },
+  {
+    id: 2,
+    category: "Css",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1646850887/Logo/css3_wbgmop.png",
+  },
+  {
+    id: 3,
+    category: "Javascript",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1646850888/Logo/javascript_1_olaeqo.png",
+  },
+  {
+    id: 4,
+    category: "Typescript",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682495503/Logo/typescript_rzrv3a.png",
+  },
+  {
+    id: 5,
+    category: "Go",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682495615/Logo/Go-Logo_Blue_zgu7vu.png",
+  },
+  {
+    id: 6,
+    category: "Uml",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682588620/Logo/plantuml_frqxjs.png",
+  },
+  {
+    id: 7,
+    category: "React",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1646850888/Logo/react_pwxdqd.png",
+  },
+  {
+    id: 8,
+    category: "Next.JS",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682586783/Logo/next-js-icon-seeklogo.com_labyxo.svg",
+  },
+  {
+    id: 9,
+    category: "Vue",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682586921/Logo/pngwing.com_swzj4c.png",
+  },
+  {
+    id: 10,
+    category: "Bootsrtap",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1646850887/Logo/bootstrap_mnwufe.png",
+  },
+  {
+    id: 11,
+    category: "Material UI",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682587086/Logo/pngwing.com_1_pt4zay.png",
+  },
+  {
+    id: 12,
+    category: "Sass",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1647956585/Logo/sass-logo_sorjxy.png",
+  },
+  {
+    id: 13,
+    category: "Tailwind",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1647956586/Logo/Tailwind_CSS_Logo.svg_ytlfw3.png",
+  },
+  {
+    id: 14,
+    category: "Framer Motion",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1647957211/Logo/60953119-d3c6f300-a2fc-11e9-9596-4978e5d52180_1_fj7xyq.png",
+  },
+  {
+    id: 15,
+    category: "Figma",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1646850887/Logo/figma_ylvvry.png",
+  },
+  {
+    id: 16,
+    category: "Node.JS",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1646850888/Logo/node-js_rmri8p.png",
+  },
+  {
+    id: 17,
+    category: "Express",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682588830/Logo/pngwing.com_2_olv4t4.png",
+  },
+  {
+    id: 18,
+    category: "Passport",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1646851026/Logo/0d184ee3-fd8d-4b94-acf4-b4e686e57375_w18gei.png",
+  },
+  {
+    id: 19,
+    category: "Axios",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682587573/Logo/57233882-20344080-6fe5-11e9-9086-d20a955bed59_gjgo5m.png",
+  },
+  {
+    id: 20,
+    category: "Socket.io",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682587521/Logo/socket-io-logo-png-transparent_ggjmnk.png",
+  },
+  {
+    id: 21,
+    category: "Mysql",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1646850887/Logo/mysql_cqq3f9.png",
+  },
+  {
+    id: 22,
+    category: "MongoDB",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682587854/Logo/mongodb_a6fimh.svg",
+  },
+  {
+    id: 23,
+    category: "Redis",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682587737/Logo/584830b5cef1014c0b5e4a9c_qoajmg.png",
+  },
+  {
+    id: 24,
+    category: "Docker",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1657952236/Logo/Docker_facebook_share_vt2fic.png",
+  },
+  {
+    id: 25,
+    category: "Gitlab",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1673156427/Logo/gitlab_mqqmxt.png",
+  },
+  {
+    id: 26,
+    category: "Kubernetes",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1657952311/Logo/Kubernetes_logo_without_workmark.svg_a9hsvx.png",
+  },
+  {
+    id: 27,
+    category: "Jenkins",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1673156118/Logo/jenkins_bix8sl.png",
+  },
+  {
+    id: 28,
+    category: "Google Clound",
+    icon: "https://res.cloudinary.com/do58tgs2e/image/upload/v1682588225/Logo/google-cloud-icon-400w_gshpe7.png",
+  },
+];
+
+export default BlogData;
